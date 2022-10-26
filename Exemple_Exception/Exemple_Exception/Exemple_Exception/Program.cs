46d16796377b98104f22b43f5dec5ec78a19e030
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:51:11 -0400

    Add files via upload

---
+++ Exemple_Exception/Exemple_Exception/Exemple_Exception/Program.cs
@@ -0,0 +1,48 @@
+﻿// Code tiré de https://www.tutorialspoint.com/csharp/csharp_exception_handling.htm
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_Exception
+{
+
+
+    class Program
+    {
+        class DivNumbers
+        {
+            int result;
+
+            public DivNumbers()
+            {
+                result = 0;
+            }
+            public void division(int num1, int num2)
+            {
+                try
+                {
+                    result = num1 / num2;
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Exception caught: {0}", e);
+                }
+                finally
+                {
+                    Console.WriteLine("Result: {0}", result);
+                }
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers();
+            d.division(25,5);
+            Console.ReadKey();
+        }
+    }
+
+}
